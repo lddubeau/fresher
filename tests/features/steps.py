67e73e5fb72176783fbde9ec1c524fbbafe8fc1e
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-09-24 20:47:05 -0400

    Invocations of nosetests in the test suite are done with -c /dev/null so as to avoid depending on the user's configuration.

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -10,7 +10,7 @@ import re
 def before(scenario):
     scc.cwd = os.getcwd()
     scc.is_traceback = False
-    
+
 @Before('traceback_not_important')
 def set_eliminate_traceback(scenario):
     scc.is_traceback = True
@@ -18,14 +18,14 @@ def set_eliminate_traceback(scenario):
 @When("^I run nose (.+)$")
 def run_nose(args):
     args_list = shlex.split(args)
-    command = ['nosetests', '--with-freshen'] + args_list 
+    command = ['nosetests', '-c', '/dev/null', '--with-freshen'] + args_list
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     scc.output, _ = process.communicate()
     scc.status = process.returncode
-    scc.output = _normalize_newlines(scc.output) 
+    scc.output = _normalize_newlines(scc.output)
     scc.output = scc.output.rstrip()
     _extract_time_and_traceback()
-    
+
 _newlines_re = re.compile(r'(\r\n|\r|\r)')
 def _normalize_newlines(string):
     return _newlines_re.sub('\n', string)
@@ -40,7 +40,7 @@ def _get_time_in_output_result(string):
     times = re.findall(_time_re, string)
     return times[0]
 
-_traceback_re = re.compile(r'Traceback(.+)' 
+_traceback_re = re.compile(r'Traceback(.+)'
                            + r'\n' + ('-' * 70),
                            re.DOTALL)
 def _get_traceback_in_output_result(string):
@@ -58,7 +58,7 @@ def check_outcome(exp_status):
 
 def _check_outcome_with(exp_output, exp_status):
     run_steps("Then it should %s" % exp_status)
-    
+
     exp_output = exp_output.replace("{cwd}", scc.cwd)
     exp_output = exp_output.replace("{time}", scc.time)
     exp_output = exp_output.replace("{sep}", os.sep)
