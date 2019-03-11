fccab7468bad8ca64e71c163d6c4fa629532a9a0
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-03-11 11:09:35 -0400

    Switch from py33 to py37 in testing.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Quality Assurance",
         "Topic :: Software Development :: Testing",
     ]
