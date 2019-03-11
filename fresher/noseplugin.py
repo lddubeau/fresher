4fba5e283b968d672a92cd98fefe2c0103bdfa85
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-03-11 11:02:20 -0400

    async is a reserved keyword in Python 3.7

--- fresher/noseplugin.py
+++ fresher/noseplugin.py
@@ -24,7 +24,7 @@ from fresher.test.base import FeatureSuite, FresherTestCase, ExceptionWrapper
 
 try:
     # use colorama for cross-platform colored text, if available
-    import colorama # pylint: disable=F0401
+    import colorama  # pylint: disable=F0401
     colorama.init()
 except ImportError:
     colorama = None
@@ -42,7 +42,8 @@ class FresherErrorPlugin(ErrorClassPlugin):
     def options(self, parser, env):
         parser.add_option('--fresher-allow-undefined',
                           action="store_true",
-                          default=env.get("NOSE_FRESHER_ALLOW_UNDEFINED") == '1',
+                          default=env.get(
+                              "NOSE_FRESHER_ALLOW_UNDEFINED") == '1',
                           dest="allow_undefined",
                           help="Allow undefined steps."
                           "[NOSE_FRESHER_ALLOW_UNDEFINED]")
@@ -58,7 +59,6 @@ class FresherErrorPlugin(ErrorClassPlugin):
               ("undefined", "UNDEFINED", not options.allow_undefined)), )
 
 
-
 class StepsLoadFailure(Failure):
 
     def __str__(self):
@@ -70,7 +70,8 @@ class ParseFailure(Failure):
         self.parse_exception = parse_exception
         self.filename = filename
         address = TestAddress(filename).totuple()
-        super(ParseFailure, self).__init__(parse_exception.__class__, parse_exception, tb, address)
+        super(ParseFailure, self).__init__(
+            parse_exception.__class__, parse_exception, tb, address)
 
     def __str__(self):
         return "Could not parse %s" % (self.filename)
@@ -80,7 +81,8 @@ class FresherNosePlugin(Plugin):
     name = "fresher"
 
     # This makes it so that fresher's formatFailure gets called before capture
-    # and logcapture - those plugins replace and obscure the true exception value
+    # and logcapture - those plugins replace and obscure the true exception
+    # value
     score = 1000
 
     def options(self, parser, env):
@@ -100,12 +102,13 @@ class FresherNosePlugin(Plugin):
                           help='Change the language used when reading the feature files')
         parser.add_option('--fresher-list-undefined',
                           action="store_true",
-                          default=env.get('NOSE_FRESHER_LIST_UNDEFINED') == '1',
+                          default=env.get(
+                              'NOSE_FRESHER_LIST_UNDEFINED') == '1',
                           dest="list_undefined",
                           help=("Make a report of all undefined steps that "
                                 "fresher encounters when running scenarios. "
                                 "(Implies --fresher-allow-undefined.) "
-                               "[NOSE_FRESHER_LIST_UNDEFINED]"))
+                                "[NOSE_FRESHER_LIST_UNDEFINED]"))
 
     def configure(self, options, config):
         super(FresherNosePlugin, self).configure(options, config)
@@ -115,7 +118,8 @@ class FresherNosePlugin(Plugin):
         self.impl_loader = StepImplLoader()
         self.topdir = os.getcwd()
         if not self.language:
-            print("Error: language '%s' not available" % options.language, file=sys.stderr)
+            print("Error: language '%s' not available" %
+                  options.language, file=sys.stderr)
             exit(1)
         if options.list_undefined:
             self.undefined_steps = []
@@ -151,7 +155,7 @@ class FresherNosePlugin(Plugin):
         on a per-feature basis."""
         if self._test_class is None:
             try:
-                from fresher.test.async import TwistedTestCase
+                from fresher.test.async_test import TwistedTestCase
                 self._test_class = TwistedTestCase
             except ImportError:
                 from fresher.test.pyunit import PyunitTestCase
@@ -159,7 +163,6 @@ class FresherNosePlugin(Plugin):
         name = feature.name.encode('utf8') if not six.PY3 else feature.name
         return type(name, (self._test_class, ),
                     {scenario.name: lambda self: self.runScenario()})
-
 
     def loadTestsFromFile(self, filename, indexes=[]):
         log.debug("Loading from file %s" % filename)
@@ -196,7 +199,7 @@ class FresherNosePlugin(Plugin):
         log.debug("Loading from name %s" % name)
 
         if not self._is_file_with_indexes(name):
-            return # let nose take care of it
+            return  # let nose take care of it
 
         name_without_indexes, indexes = self._split_file_in_indexes(name)
         if not os.path.exists(name_without_indexes):
@@ -258,6 +261,7 @@ class FresherNosePlugin(Plugin):
         # UndefinedStepImpl exceptions for reporting later.
         if self.undefined_steps is not None:
             plugin = self
+
             def _addError(self, test, err):
                 ec, ev, tb = err
                 if isclass(ec) and issubclass(ec, UndefinedStepImpl):
@@ -268,12 +272,15 @@ class FresherNosePlugin(Plugin):
 
     def report(self, stream):
         if self.undefined_steps:
-            stream.write("======================================================================\n")
+            stream.write(
+                "======================================================================\n")
             stream.write("Tests with undefined steps\n")
-            stream.write("----------------------------------------------------------------------\n")
+            stream.write(
+                "----------------------------------------------------------------------\n")
             for test, ec, ev, tb in self.undefined_steps:
                 stream.write(self._formatSteps(test, ev.step, False) + "\n\n")
-            stream.write("You can implement step definitions for the missing steps with these snippets:\n\n")
+            stream.write(
+                "You can implement step definitions for the missing steps with these snippets:\n\n")
             uniq_steps = set(s[2].step for s in self.undefined_steps)
             for step in uniq_steps:
                 stream.write('@%s(r"^%s$")\n' % (self.language.words(step.step_type)[0],
